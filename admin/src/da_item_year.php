0709bc346c452ef1b904f7ccd38a72cab7541669
BuildTools <unconfigured@null.spigotmc.org>
2023-04-15 21:46:30 +0700

    sleepz

--- admin/src/da_item_year.php
+++ admin/src/da_item_year.php
@@ -1,9 +1,4 @@
 <?php
-session_start(); // เปิดใช้งาน session
-if (!isset($_SESSION['account'])) { // ถ้าไม่เข้าระบบอยู่
-    header("location: ../auth/login.php"); // redirect ไปยังหน้า login.php
-    exit;
-}
 include_once("./connect.php");
 $conn = connectDB();
 $sql = "SELECT da_id, da_status_ii, ADDDATE(da_date, INTERVAL 1 YEAR) AS Date_NEW FROM da_items";
