fa48465c99e1f564bebcc7ac8333d2a34d0641e7
BuildTools <unconfigured@null.spigotmc.org>
2023-04-18 20:10:20 +0700

    sleepz

--- admin/src/js/da_item_table_json.js
+++ admin/src/js/da_item_table_json.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         },
         columns: [
             {data: 'da_id', render: function (da_id) {
-                return '<input type="checkbox" name="checkbox_da_id[]" id="checkbox_da_id" value="' + "'" + da_id + "'" + '">';
+                return '<input type="checkbox" name="checkbox_da_id[]" id="checkbox_da_id" value="' + da_id + '">';
             }},
             {
                 data: '',
