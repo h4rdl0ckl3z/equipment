11390a2148adf29d9ecb782b11f46c5f9fb22ace
BuildTools <unconfigured@null.spigotmc.org>
2023-07-06 14:36:39 +0700

    sleepz

--- admin/src/js/doc_da_item_sale_table_json.js
+++ admin/src/js/doc_da_item_sale_table_json.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+    var agency_id = document.getElementById('agency_id');
+    var access_level = document.getElementById('access_level');
     var table = $('#Da_ItemTable').DataTable({
         ajax: {
             url: './src/doc_da_item_sale_select.php',
-            method: 'post'
+            method: 'post',
+            data: {
+                agency_id: agency_id.value,
+                access_level: access_level.value
+            }
         },
         columns: [
             {
