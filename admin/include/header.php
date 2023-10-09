c3f95cb43d677b20bd497226be81f61c45f0f1c6
h65rdl0ckl3z <bank427ingza@gmail.com>
2023-10-09 11:39:02 +0700

    SleepZ

--- admin/include/header.php
+++ admin/include/header.php
@@ -62,7 +62,7 @@
       </ul>
 
       <!-- Right navbar links -->
-      <ul class="navbar-nav ml-auto">
+      <!-- <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" data-widget="fullscreen" href="#" role="button">
             <i class="fas fa-expand-arrows-alt"></i>
@@ -73,7 +73,7 @@
             <i class="fas fa-th-large"></i>
           </a>
         </li>
-      </ul>
+      </ul> -->
     </nav>
     <!-- /.navbar -->
 
