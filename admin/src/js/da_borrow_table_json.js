0abd8aebff3ae142e99c593ec23a5fbda22401b1
h65rdl0ckl3z <bank427ingza@gmail.com>
2023-10-09 11:44:16 +0700

    SleepZ

--- admin/src/js/da_borrow_table_json.js
+++ admin/src/js/da_borrow_table_json.js
@@ -417,6 +417,29 @@ if (access_level == 0 || access_level == 2) {
                     }
                 }},
                 {data: 'da_lists'},
+                {data: 'da_br_status', render: function (da_status_i) {
+                    if (da_status_i == '0') {
+                        return 'ปกติ';
+                    } else if (da_status_i == '1') {
+                        return 'ชำรุด';
+                    } else if (da_status_i == '2') {
+                        return 'เสื่อมคุณถาพ';
+                    } else {
+                        return 'สูญหาย';
+                    }
+                }},
+                {data: 'da_status_i', render: function (da_status_i) {
+                    check_da_status_i = da_status_i
+                    if (da_status_i == '0') {
+                        return 'ปกติ';
+                    } else if (da_status_i == '1') {
+                        return 'ชำรุด';
+                    } else if (da_status_i == '2') {
+                        return 'เสื่อมคุณถาพ';
+                    } else {
+                        return 'สูญหาย';
+                    }
+                }},
                 {data: 'da_br_location'},
                 {data: 'da_borrow', render: function (da_borrow) {
                     function toThaiDateString(date) {
@@ -461,6 +484,20 @@ if (access_level == 0 || access_level == 2) {
                     }
                     let date = new Date(da_return);
                     return toThaiDateString(date);
+                }},
+                {data: 'da_status_ii', visible: false, render: function (da_status_ii) {
+                    check_da_status_ii = da_status_ii
+                    if (da_status_ii == '0') {
+                        return 'ปกติ';
+                    } else if (da_status_ii == '1') {
+                        return 'ยืม';
+                    } else if (da_status_ii == '2') {
+                        return 'แจ้งซ่อม';
+                    } else if (da_status_ii == '3') {
+                        return 'การตัดจำหน่าย';
+                    } else {
+                        return 'ตรวจสอบสภาพ';
+                    }
                 }},
                 {data: 'allow_br', render: function (allow_br) {
                     check_allow_br = allow_br
