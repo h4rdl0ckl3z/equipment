35c6858810bd1d45a2a1364d76702027405162ec
h65rdl0ckl3z <bank427ingza@gmail.com>
2023-10-14 21:28:50 +0700

    SleepZ

--- admin/src/da_item_year.php
+++ admin/src/da_item_year.php
@@ -6,12 +6,14 @@ $da_id = (isset($_POST["checkbox_da_id"]))?$_POST['checkbox_da_id']:NULL;
 if ($da_id <> '') {
     if ($da_id[0] == 'on') {
       unset($da_id[0]);
-      print_r($da_id);
-      $sql = "";
+      $extract_id = implode(', ', $da_id);
+      $sql = "UPDATE da_items SET da_status_ii = '4' WHERE da_id IN ($extract_id)";
     } else {
-
+      $extract_id = implode(', ', $da_id);
+      // echo $extract_id;
+      $sql = "UPDATE da_items SET da_status_ii = '4' WHERE da_id IN ($extract_id)";
     }
-    // $conn->query($sql);
-    // $conn->close();
+    $conn->query($sql);
+    $conn->close();
   }
 ?>
